e3a54bdaa1d72faa3695c0bfd2ff4bc80e75c118
Marek Trybuła <trybu001@gmail.com>
2024-02-13 08:23:00 +0100

    Maximum Aavarge Subarray Sliding Window approach

---
+++ 643_MaximumAvgSubarrayI.js
@@ -0,0 +1,24 @@
+var findMaxAverage = function (nums, k) {
+  let avg
+  let sum = 0
+
+  for (let i = 0; i < k; i++) {
+    sum += nums[i]
+  }
+  console.log(`Pierwsza suma równa się ${sum}`)
+  avg = sum / k
+
+  for (let right = k; right < nums.length; right++) {
+    console.log(
+      `${sum} = ${nums[right]} - ${nums[right - k + 1]} - ${nums[right - k]}`,
+    )
+    sum = sum - nums[right - k] + nums[right]
+
+    if (sum / k > avg) avg = sum / k
+  }
+
+  return avg
+}
+
+console.log(findMaxAverage([1, 12, -5, -6, 50, 3], 4))
+console.log(findMaxAverage([5], 1))
